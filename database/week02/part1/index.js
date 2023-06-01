8d6c0a428a5b23d6013a1ae2dfe04ec3cf233b22
Adeeba <124208667+AdeebaSaeed@users.noreply.github.com>
2023-06-01 17:08:38 +0200

    Update index.js

--- database/week02/part1/index.js
+++ database/week02/part1/index.js
@@ -1,4 +1,178 @@
+// this is new code
+const mysql = require('mysql2');
 
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Adeeba@777',
+  database: 'new_world'
+});
+
+// Connect to the MySQL server
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL server:', err);
+    return;
+  }
+
+  // Prompt for user input
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  // Prompt the user for query choice and input
+  readline.question('Choose a query (1-4):\n1. What is the capital of country X?\n2. List all the languages spoken in the region Y\n3. Find the number of cities in which language Z is spoken\n4. List all the continents with the number of languages spoken in each continent\n\nEnter your choice: ', (choice) => {
+    switch (choice) {
+      case '1':
+        readline.question('Enter the country name: ', (countryName) => {
+          getCapitalOfCountry(countryName);
+          closeConnection();
+        });
+        break;
+      case '2':
+        readline.question('Enter the region name: ', (region) => {
+          getLanguagesInRegion(region);
+          closeConnection();
+        });
+        break;
+      case '3':
+        readline.question('Enter the language: ', (language) => {
+          countCitiesWithLanguage(language);
+          closeConnection();
+        });
+        break;
+      case '4':
+        countLanguagesPerContinent();
+        closeConnection();
+        break;
+      default:
+        console.log('Invalid choice. Exiting...');
+        closeConnection();
+    }
+  });
+
+});
+
+// Get the capital of a country
+function getCapital(countryName) {
+  const query = 'SELECT city.name AS capital FROM country INNER JOIN city ON country.capital = city.id WHERE country.name = ?';
+
+  connection.prepare(query, (err, statement) => {
+    if (err) {
+      console.error('Error preparing statement:', err);
+      return;
+    }
+
+    statement.execute([countryName], (err, rows, columns) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        return;
+      }
+
+      if (rows.length === 0) {
+        console.log('Country not found');
+      } else {
+        const capital = rows[0].capital;
+        console.log(`Capital of ${countryName}: ${capital}`);
+      }
+
+      statement.close();
+    });
+  });
+}
+
+// List all languages spoken in a region
+function listLanguagesInRegion(regionName) {
+  const query = 'SELECT DISTINCT cl.language FROM country c  INNER JOIN countrylanguage cl ON c.code = cl.countrycode WHERE c.region = ?';
+
+  connection.prepare(query, (err, statement) => {
+    if (err) {
+      console.error('Error preparing statement:', err);
+      return;
+    }
+
+    statement.execute([regionName], (err, rows, columns) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        return;
+      }
+
+      if (rows.length === 0) {
+        console.log('No languages found');
+      } else {
+        const languages = rows.map((row) => row.language);
+        console.log(`Languages spoken in ${regionName}: ${languages.join(', ')}`);
+      }
+
+      statement.close();
+    });
+  });
+}
+
+// Get the number of cities where a language is spoken
+function countCitiesWithLanguage(language) {
+  const query = 'SELECT COUNT(DISTINCT city.id) AS cityCount FROM city INNER JOIN countrylanguage ON city.CountryCode = countrylanguage.CountryCode WHERE countrylanguage.Language = ?';
+
+  connection.prepare(query, (err, statement) => {
+    if (err) {
+      console.error('Error preparing statement:', err);
+      return;
+    }
+
+    statement.execute([language], (err, rows, columns) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        return;
+      }
+
+      const cityCount = rows[0].cityCount;
+      console.log(`Number of cities where ${language} is spoken: ${cityCount}`);
+
+      statement.close();
+    });
+  });
+}
+
+// List all continents with the number of languages spoken in each continent
+function listContinentsWithLanguageCount() {
+  const query = 'SELECT continent, COUNT(DISTINCT language) AS language_count FROM country INNER JOIN countrylanguage ON country.Code = countrylanguage.CountryCode GROUP BY continent';
+
+  connection.prepare(query, (err, statement) => {
+    if (err) {
+      console.error('Error preparing statement:', err);
+      return;
+    }
+
+    statement.execute((err, rows, columns) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        return;
+      }
+
+      rows.forEach((row) => {
+        console.log(`${row.continent}: ${row.language_count} languages`);
+      });
+
+      statement.close();
+    });
+  });
+}
+
+// Close the database connection
+function closeConnection() {
+  connection.end((err) => {
+    if (err) {
+      console.error('Error closing connection:', err);
+    }
+  });
+}
+
+
+
+
+// hre end
  
 //code for node js program to run prepared statements
  const mysql = require('mysql2');
